ff4f74b99d897a59979d6c33c999a91b237b5892
JaysonD <jaduff20@gmail.com>
2020-04-09 17:19:05 -0400

    refactored to burger code, create and delete burger works, code displays on localhost, eaten/not eaten does not work

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,6 @@
-INSERT INTO cats (name) VALUES ('Meeses');
-INSERT INTO cats (name) VALUES ('Bobbi');
-INSERT INTO cats (name, sleepy) VALUES ('Sylvester', true);
-INSERT INTO cats (name, sleepy) VALUES ('Marilyn Manson', true);
-INSERT INTO cats (name, sleepy) VALUES ('Joe Biden', true);
-INSERT INTO cats (name) VALUES ('Owens');
+INSERT INTO burgers (name) VALUES ('Bacon Cheeseburger');
+INSERT INTO burgers (name) VALUES ('Quarter Pounder');
+INSERT INTO burgers (name, eaten) VALUES ('Royale with Cheese', true);
+INSERT INTO burgers (name, eaten) VALUES ('THICC Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('Baconator', true);
+INSERT INTO burgers (name) VALUES ('Triple Coronary Bypass');
